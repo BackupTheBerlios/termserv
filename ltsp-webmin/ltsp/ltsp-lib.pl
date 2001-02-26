4571eb058c6fce58a108950a9c3c115e19d0bc0a
sledge <sledge>
2001-02-26 18:19:21 +0000

    non-file-rewriting ltsp_write_config. doesn't work fully yet.

--- ltsp-webmin/ltsp/ltsp-lib.pl
+++ ltsp-webmin/ltsp/ltsp-lib.pl
@@ -1,12 +1,21 @@
 #!/usr/bin/perl
 
-$DEBUG = 0;
+$DEBUG = 1;
 
 # Hash "profiles"
 # Contains the host configuration entry name as key
 # and the configuration values as "conf,value;conf,value"
 
 %profiles = ();
+
+# Lists "added_hosts", "modified_hosts" and "deleted_hosts
+# contain new, modified or deleted hosts; these lists
+# are needed for simpler non-rewriting configuration file
+# operations
+
+@added_hosts = ();
+@modified_hosts = ();
+@deleted_hosts = ();
 
 # Reads the configuration file
 # puts information into %profiles
@@ -67,27 +76,79 @@ sub ltsp_write_config($) {
 
   print "ltsp_write_config: procedure called\n<br>" if $DEBUG;
 
-  my $conf_file = shift(@_);
-  &lock_file("$conf_file");
-  open (LST, ">$conf_file");
+  my $config_file = shift(@_);
 
-  foreach (&ltsp_get_hces()) {
-    my $cur_hce = $_;
-    if (&ltsp_get_configuration($cur_hce) != 0) {
-      %conf = &ltsp_get_configuration($cur_hce);
-      print "ltsp_write_config: hce is $cur_hce\n<br>" if $DEBUG;
-      print LST "[$cur_hce]\n";
-      print "<tt>LST: [$cur_hce]</tt><br>\n" if $DEBUG;
-      foreach (keys(%conf)) {
-        print LST "$_ = \"" . $conf{"$_"} . "\"\n";
-        print "<tt>LST $_ = \"" . $conf{"$_"} . "\"</tt><br>\n" if $DEBUG;
-      } 
+  print "ltsp_write_config: reading configuration file\n<br>" if $DEBUG;
+
+  &lock_file("$config_file");
+  open (LST, "<$config_file");
+  @lines = (<LST>);
+  close (LST);
+  &unlock_file("$config_file");
+  &webmin_log("read", "file", $config_file, );
+
+  # We'll do a test drive to stdout
+
+  # Do we have any hosts deleted?
+  print "ltsp_write_config: hosts deleted: $#deleted_hosts, @deleted_hosts<br>\n" if $DEBUG;
+  if ($#deleted_hosts != -1) {
+    foreach (@deleted_hosts) {
+      $i = 0;
+
+      print "ltsp_write_config: looking for $_<br>\n" if $DEBUG;
+      # Find out where to begin deletion
+      BEGINDELETE: for ($i = 0; $i < $#lines; $i++) {
+        if ($lines[$i] =~ /^\[$_\]/) {
+          print "ltsp_write_config: begin delete at $i<br>\n" if $DEBUG;
+          $begin_delete = $i;
+          last BEGINDELETE;
+        }
+      }
+      # Find out where to stop deletion
+      ENDDELETE: while ($i < $#lines) {
+        $i++;
+        if ($lines[$i] =~ /^\[(.*)?\]/) {
+          print "ltsp_write_config: end delete at $i<br>\n" if $DEBUG;
+          $end_delete = $i-1;
+          last ENDDELETE;
+        }
+      }
+      # In case of EOF
+      if ($end_delete != $i) { $end_delete = $i; }
+      print "ltsp_write_config: delete lines $begin_delete to $end_delete\n<br>" if $DEBUG;
     }
   }
 
-  close (LST);
-  &unlock_file("$conf_file");
-  &webmin_log("write", "file", $conf_file, );
+  # Do we have any new hosts?
+  if ($#added_hosts ne -1) {
+    foreach (@added_hosts) {
+      my $cur_hce = $_;
+      if (&ltsp_get_configuration($cur_hce) != 0) {
+        %conf = &ltsp_get_configuration($cur_hce);
+        print "ltsp_write_config: added hce is $cur_hce\n<br>" if $DEBUG;
+        #print LST "[$cur_hce]\n";
+        print "<tt>added LST: [$cur_hce]</tt><br>\n" if $DEBUG;
+        foreach (keys(%conf)) {
+          #print LST "$_ = \"" . $conf{"$_"} . "\"\n";
+          print "<tt>added LST $_ = \"" . $conf{"$_"} . "\"</tt><br>\n" if $DEBUG;
+        } 
+      }
+    }
+  }
+
+  #&lock_file("$conf_file");
+  #open (LST, ">$conf_file");
+
+  # Insert file writing operation code here
+
+  #close (LST);
+  #&unlock_file("$conf_file");
+  #&webmin_log("write", "file", $conf_file, );
+
+  # Set back "helper" variables
+  @added_hosts = ();
+  @modified_hosts = ();
+  @deleted_hosts = ();
 
   print "ltsp_write_config: procedure finished\n<br>" if $DEBUG;
 
@@ -205,6 +266,7 @@ sub ltsp_modify_entry($, %) {
   # so it's been created
 
   if ($profiles{"$entry"} eq "") {
+    $newhost = 1;
     push (@added_hosts, $entry);
     print "ltsp_modify_entry: $entry is a new entry<br>\n" if $DEBUG;
   }
@@ -216,12 +278,17 @@ sub ltsp_modify_entry($, %) {
   print "ltsp_modify_entry: $entry is " . $profiles{"$entry"} . "<br>\n" if $DEBUG;
   $profiles{"$entry"} = substr($profiles{"$entry"}, 1);
 
+  if (!($newhost eq 1) && !($profiles{"$entry"} eq "")) {
+    push (@modified_hosts, $entry);
+    print "ltsp_modify_entry: $entry is a modified entry<br>\n" if $DEBUG;
+  }
+
   # Test whether the entry is empty at the end
   # empty means that the entry does not have any configuration
   # so the user wanted to delete that entry
 
   if ($profiles{"$entry"} eq "") {
-    push (@added_hosts, $entry);
+    push (@deleted_hosts, $entry);
     print "ltsp_modify_entry: $entry is a deleted entry<br>\n" if $DEBUG;
   }
 
