d9fea4e143ec6568020fa10afa9b95d9cc9f1012
baum <baum>
2002-03-14 21:42:27 +0000

    OOps - the initial commit was wrong. Removed the unnecessary files.

--- ltsp/ltsp-webmin/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ltsp-webmin package
-#
-0 4	* * *	root	ltsp-webmin_maintenance
--- ltsp/ltsp-webmin/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ltsp-webmin):needs=X11|text|vc|wm section=Apps/see-menu-manual\
-  title="ltsp-webmin" command="/usr/bin/ltsp-webmin"
